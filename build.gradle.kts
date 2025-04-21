e56373dd18f089ad4046257d51bd62781fae0e8d
Bawnorton <bawnorton@me.com>
2025-04-21 10:42:34 -0700

    bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
     id("org.jetbrains.intellij") version "1.16.1"
 }
 
